d98eb3e14a70a91d9eb5e3ab222057f3b32fe126
kennyyiu <kenny.kwyiu@gmail.com>
2025-07-01 22:53:34 +0800

    add rememberMe

--- src/main/java/com/kenny/uaa/config/SecurityConfig.java
+++ src/main/java/com/kenny/uaa/config/SecurityConfig.java
@@ -1,12 +1,15 @@
 package com.kenny.uaa.config;
 
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -16,7 +19,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests(req -> req.anyRequest().authenticated())
                 .formLogin(form -> form.loginPage("/login").permitAll())
 //                .httpBasic(Customizer.withDefaults())
-                .csrf(Customizer.withDefaults());
+                .csrf(Customizer.withDefaults())
+                .logout(logout -> logout.logoutUrl("/perform_logout"))
+                .rememberMe(rememberMe -> rememberMe.tokenValiditySeconds(30*24*3600).rememberMeCookieName("someKeyToRemember"));
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password(passwordEncoder().encode("123456"))
+                .roles("USER","ADMIN");
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Override
