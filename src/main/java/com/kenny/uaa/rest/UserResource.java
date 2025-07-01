f5e3597ab140ce1889397991531034534f58aeed
kennyyiu <kenny.kwyiu@gmail.com>
2025-07-01 18:05:16 +0800

    add put and post api

--- src/main/java/com/kenny/uaa/rest/UserResource.java
+++ src/main/java/com/kenny/uaa/rest/UserResource.java
@@ -1,5 +1,6 @@
 package com.kenny.uaa.rest;
 
+import lombok.Data;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,7 +13,18 @@ public class UserResource {
     }
 
     @PostMapping("/greeting")
-    public String makeGreeting(@RequestParam String name) {
+    public String makeGreeting(@RequestParam String name, @RequestBody Profile profile) {
+        return "Hello " + name + "!\n" + profile.gender;
+    }
+
+    @PutMapping("/greeting/{name}")
+    public String putGreeting(@PathVariable String name) {
         return "Hello " + name + "!";
+    }
+
+    @Data
+    static class Profile {
+        private String gender;
+        private String idNo;
     }
 }
