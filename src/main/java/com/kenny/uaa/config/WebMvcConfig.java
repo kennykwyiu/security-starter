1e9e948b97252de4fa24ed0476827f4f35bf070e
kennyyiu <kenny.kwyiu@gmail.com>
2025-07-01 20:24:47 +0800

    add WebMvcConfig

---
+++ src/main/java/com/kenny/uaa/config/WebMvcConfig.java
@@ -0,0 +1,25 @@
+package com.kenny.uaa.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("/webjars/")
+                .resourceChain(false);
+        registry.setOrder(1);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+        registry.setOrder(HIGHEST_PRECEDENCE);
+    }
+}
