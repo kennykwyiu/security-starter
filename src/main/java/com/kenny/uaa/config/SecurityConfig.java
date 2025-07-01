e51f212cc14f6950c6b76b9c468489b234a97da9
kennyyiu <kenny.kwyiu@gmail.com>
2025-07-01 20:24:34 +0800

    revise SecurityConfig

--- src/main/java/com/kenny/uaa/config/SecurityConfig.java
+++ src/main/java/com/kenny/uaa/config/SecurityConfig.java
@@ -1,17 +1,27 @@
 package com.kenny.uaa.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf-> csrf.disable())
-                .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults())
-                    .authorizeRequests(req -> req.antMatchers("/api/**").authenticated());
+                .authorizeRequests(req -> req.anyRequest().authenticated())
+                .formLogin(form -> form.loginPage("/login").permitAll())
+//                .httpBasic(Customizer.withDefaults())
+                .csrf(csrf-> csrf.disable());
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/public/**")
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 }
