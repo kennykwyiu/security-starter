56bcbbffbcb1178d5905aaed7412ef7b013f3240
kennyyiu <kenny.kwyiu@gmail.com>
2025-07-02 21:50:27 +0800

    add error.html

--- src/main/java/com/kenny/uaa/config/WebMvcConfig.java
+++ src/main/java/com/kenny/uaa/config/WebMvcConfig.java
@@ -21,6 +21,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/").setViewName("index");
+        registry.addViewController("/error").setViewName("error");
         registry.setOrder(HIGHEST_PRECEDENCE);
     }
 }
