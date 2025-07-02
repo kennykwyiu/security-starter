6659023b8378c19cc5cf6650b20c0e93a94c2365
kennyyiu <kenny.kwyiu@gmail.com>
2025-07-02 22:05:28 +0800

    add .successHandler(jsonLoginSuccessHandler())

--- src/main/java/com/kenny/uaa/config/SecurityConfig.java
+++ src/main/java/com/kenny/uaa/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.kenny.uaa.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,7 +13,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
+@Slf4j
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
@@ -22,11 +27,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin(form -> form
                     .loginPage("/login")
                     .defaultSuccessUrl("/", true)
+                    .successHandler(jsonLoginSuccessHandler())
                     .permitAll())
 //                .httpBasic(Customizer.withDefaults())
                 .csrf(Customizer.withDefaults())
                 .logout(logout -> logout.logoutUrl("/perform_logout"))
                 .rememberMe(rememberMe -> rememberMe.tokenValiditySeconds(30*24*3600).rememberMeCookieName("someKeyToRemember"));
+    }
+
+    private AuthenticationSuccessHandler jsonLoginSuccessHandler() {
+        return (request, response, authentication) -> {
+            ObjectMapper objectMapper = new ObjectMapper();
+            response.setStatus(HttpStatus.OK.value());
+            response.getWriter().println(objectMapper.writeValueAsString(authentication));
+            log.debug("Auth successful");
+        };
     }
 
     @Override
