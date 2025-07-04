ff24bf05efd9a4aa1eb010d512fa8a71d27fcd36
kennyyiu <kenny.kwyiu@gmail.com>
2025-07-04 23:05:25 +0800

    add ValidEmail annotation

---
+++ src/main/java/com/kenny/uaa/annotation/ValidEmail.java
@@ -0,0 +1,17 @@
+package com.kenny.uaa.annotation;
+
+import com.kenny.uaa.validation.EmailValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Target({ ElementType.TYPE, ElementType.FIELD, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Constraint(validatedBy = EmailValidator.class)
+public @interface ValidEmail {
+    String message() default "Invalid email";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
