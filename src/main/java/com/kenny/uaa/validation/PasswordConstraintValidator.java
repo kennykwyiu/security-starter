9f4939a02bbbd6875e7819d7670cbf4c1d53ea01
kennyyiu <kenny.kwyiu@gmail.com>
2025-07-05 09:52:46 +0800

    add PasswordConstraintValidator

---
+++ src/main/java/com/kenny/uaa/validation/PasswordConstraintValidator.java
@@ -0,0 +1,37 @@
+package com.kenny.uaa.validation;
+
+import com.kenny.uaa.annotation.ValidPassword;
+import org.passay.*;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.Arrays;
+
+public class PasswordConstraintValidator implements ConstraintValidator<ValidPassword, String> {
+
+    @Override
+    public void initialize(ValidPassword constraintAnnotation) {
+    }
+
+    @Override
+    public boolean isValid(String password, ConstraintValidatorContext constraintValidatorContext) {
+        PasswordValidator validator = new PasswordValidator(Arrays.asList(
+                new LengthRule(8, 30),
+                new CharacterRule(EnglishCharacterData.UpperCase, 1),
+                new CharacterRule(EnglishCharacterData.LowerCase, 1),
+                new CharacterRule(EnglishCharacterData.Digit, 1),
+                new CharacterRule(EnglishCharacterData.Special, 1),
+                new IllegalSequenceRule(EnglishSequenceData.Alphabetical, 5, false),
+                new IllegalSequenceRule(EnglishSequenceData.Numerical, 5, false),
+                new IllegalSequenceRule(EnglishSequenceData.USQwerty, 5, false),
+                new WhitespaceRule()
+        ));
+        RuleResult result = validator.validate(new PasswordData(password));
+        if (result.isValid()) {
+            return true;
+        }
+        constraintValidatorContext.disableDefaultConstraintViolation();
+        constraintValidatorContext.buildConstraintViolationWithTemplate(String.join(",", validator.getMessages(result)))
+                .addConstraintViolation();
+        return false;
+    }
+}
