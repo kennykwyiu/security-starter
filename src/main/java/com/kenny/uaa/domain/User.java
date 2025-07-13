f8353f18d586267b36fdcf7980bcdacd4c3b920f
kennyyiu <kenny.kwyiu@gmail.com>
2025-07-13 13:45:23 +0800

    revised User

--- src/main/java/com/kenny/uaa/domain/User.java
+++ src/main/java/com/kenny/uaa/domain/User.java
@@ -1,13 +1,46 @@
 package com.kenny.uaa.domain;
 
 import lombok.Data;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 
 @Data
-public class User implements Serializable {
+@Entity
+@Table(name = "mooc_users")
+public class User implements UserDetails, Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 80, unique = true, nullable = false)
     private String username;
+    @Column(name = "password_hash", length = 100, nullable = false)
     private String password;
+    @Column(length = 11, unique = true, nullable = false)
+    private String mobile;
+    @Column(unique = true, nullable = false)
     private String email;
+    @Column(length = 50)
     private String name;
+    private boolean enabled;
+    @Column(name = "account_non_expired", nullable = false)
+    private boolean accountNonExpired;
+    @Column(name = "account_non_locked", nullable = false)
+    private boolean accountNonLocked;
+    @Column(name = "credentials_non_expired", nullable = false)
+    private boolean credentialsNonExpired;
+    @ManyToMany
+    @Fetch(FetchMode.JOIN)
+    @JoinTable(name = "mooc_users_roles",
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "id")})
+    private Set<Role> authorities;
+
 }
