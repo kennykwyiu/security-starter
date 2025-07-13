8f05bed9ba0ee4c28581854bb3f056c24a801c2e
kennyyiu <kenny.kwyiu@gmail.com>
2025-07-13 13:45:31 +0800

    Role

---
+++ src/main/java/com/kenny/uaa/domain/Role.java
@@ -0,0 +1,23 @@
+package com.kenny.uaa.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "mooc_roles")
+public class Role implements GrantedAuthority, Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "role_name", unique = true, nullable = false, length = 50)
+    private String authority;
+}
