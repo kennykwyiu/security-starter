0b5a3d7ed58ee5916e838d4e4a8c59929f91d251
kennyyiu <kenny.kwyiu@gmail.com>
2025-07-14 21:28:29 +0800

    updatePassword

--- src/main/java/com/kenny/uaa/security/userdetails/UserDetailsPasswordServiceImpl.java
+++ src/main/java/com/kenny/uaa/security/userdetails/UserDetailsPasswordServiceImpl.java
@@ -1,14 +1,23 @@
 package com.kenny.uaa.security.userdetails;
 
+import com.kenny.uaa.repository.UserRepo;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.UserDetailsPasswordService;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
-public class UserDetailsPasswordServiceImpl implements UserDetailsService {
+public class UserDetailsPasswordServiceImpl implements UserDetailsPasswordService {
+
+    private final UserRepo userRepo;
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails updatePassword(UserDetails userDetail, String newPassword) {
+        return userRepo.findOptionalByUsername(userDetail.getUsername())
+                .map(user -> {
+                    return (UserDetails) userRepo.save(user.withPassword(newPassword));
+                })
+                .orElse(userDetail);
     }
 }
