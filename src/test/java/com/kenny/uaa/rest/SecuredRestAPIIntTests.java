5de56424ad7fd88e8a8c4cede489a3f96eeeac16
kennyyiu <kenny.kwyiu@gmail.com>
2025-07-17 20:57:04 +0800

    add giveAuthRequest_shouldSucceedWith200

---
+++ src/test/java/com/kenny/uaa/rest/SecuredRestAPIIntTests.java
@@ -0,0 +1,32 @@
+package com.kenny.uaa.rest;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootTest
+public class SecuredRestAPIIntTests {
+    @Autowired
+    private WebApplicationContext context;
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @Test
+    public void giveAuthRequest_shouldSucceedWith200() throws Exception {
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/api/greeting")
+        )
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+}
