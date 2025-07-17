d7b04c53519fbecb5bdc60ec89f7e2b52a38fb13
kennyyiu <kenny.kwyiu@gmail.com>
2025-07-17 20:38:03 +0800

    add schema.sql

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,54 @@
+/*DROP TABLE IF EXISTS mooc_users;
+CREATE TABLE mooc_users
+(
+    username VARCHAR(50)  NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    enabled  TINYINT      NOT NULL DEFAULT 1,
+    name VARCHAR(50)      NULL,
+    PRIMARY KEY (username)
+) ENGINE = INNODB;
+
+DROP TABLE IF EXISTS mooc_authorities;
+CREATE TABLE mooc_authorities
+(
+    username  VARCHAR(50) NOT NULL,
+    authority VARCHAR(50) NOT NULL,
+    CONSTRAINT fk_authorities_users
+        FOREIGN KEY (username)
+            REFERENCES mooc_users (username)
+) ENGINE=InnoDB;
+
+CREATE UNIQUE INDEX ix_auth_username on mooc_authorities (username, authority);*/
+
+
+CREATE TABLE IF NOT EXISTS mooc_roles (
+                                          id BIGINT NOT NULL AUTO_INCREMENT,
+                                          role_name VARCHAR(50) NOT NULL,
+                                          PRIMARY KEY (id),
+                                          CONSTRAINT uk_mooc_roles_role_name UNIQUE (role_name)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS mooc_users (
+                                          id BIGINT NOT NULL AUTO_INCREMENT,
+                                          account_non_expired BIT NOT NULL,
+                                          account_non_locked BIT NOT NULL,
+                                          credentials_non_expired BIT NOT NULL,
+                                          email VARCHAR(254) NOT NULL,
+                                          enabled BIT NOT NULL,
+                                          mobile VARCHAR(11) NOT NULL,
+                                          name VARCHAR(50) NOT NULL,
+                                          password_hash VARCHAR(80) NOT NULL,
+                                          username VARCHAR(50) NOT NULL,
+                                          PRIMARY KEY (id),
+                                          CONSTRAINT uk_mooc_users_username UNIQUE (username),
+                                          CONSTRAINT uk_mooc_users_mobile UNIQUE (mobile),
+                                          CONSTRAINT uk_mooc_users_email UNIQUE (email)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS mooc_users_roles (
+                                                user_id BIGINT NOT NULL,
+                                                role_id BIGINT NOT NULL,
+                                                PRIMARY KEY (user_id, role_id),
+                                                CONSTRAINT fk_users_roles_user_id_mooc_users_id FOREIGN KEY (user_id) REFERENCES mooc_users (id),
+                                                CONSTRAINT fk_users_roles_role_id_mooc_roles_id FOREIGN KEY (role_id) REFERENCES mooc_roles (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
