80f1da152532d9cde3bc809f203fdc5cc0259857
kennyyiu <kenny.kwyiu@gmail.com>
2025-07-19 16:06:29 +0800

    add giveUsernameAndPassword_ThenFindUserSuccess

---
+++ src/test/java/com/kenny/uaa/security/auth/ldap/LDAPUserRepoIntTests.java
@@ -0,0 +1,28 @@
+package com.kenny.uaa.security.auth.ldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.ldap.DataLdapTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+@DataLdapTest
+@ActiveProfiles("test")
+public class LDAPUserRepoIntTests {
+    @Autowired
+    private LDAPUserRepo userRepo;
+
+    @Test
+    public void giveUsernameAndPassword_ThenFindUserSuccess() {
+        String username = "kenny";
+        String password = "123456";
+        Optional<LDAPUser> oUser = userRepo.findByUsernameAndPassword(username, password);
+        assertTrue(oUser.isPresent());
+    }
+
+}
