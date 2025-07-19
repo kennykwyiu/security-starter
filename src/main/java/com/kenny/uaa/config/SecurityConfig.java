3c50453e41a5d262d32c5e864653e99365187ffc
kennyyiu <kenny.kwyiu@gmail.com>
2025-07-19 22:07:25 +0800

    UserDetailsPasswordServiceImpl

--- src/main/java/com/kenny/uaa/config/SecurityConfig.java
+++ src/main/java/com/kenny/uaa/config/SecurityConfig.java
@@ -1,16 +1,13 @@
 package com.kenny.uaa.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.kenny.uaa.security.filter.RestAuthenticationFilter;
+import com.kenny.uaa.security.userdetails.UserDetailsPasswordServiceImpl;
 import com.kenny.uaa.security.userdetails.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,18 +16,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.zalando.problem.spring.web.advice.security.SecurityProblemSupport;
 
-import javax.sql.DataSource;
 import java.util.Map;
 @RequiredArgsConstructor
 @Slf4j
@@ -38,9 +29,10 @@ import java.util.Map;
 @Import(SecurityProblemSupport.class)
 @Order(99)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    private final ObjectMapper objectMapper;
+
     private final SecurityProblemSupport securityProblemSupport;
     private final UserDetailsServiceImpl userDetailsService;
+    private final UserDetailsPasswordServiceImpl userDetailsPasswordService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -57,59 +49,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         .antMatchers("/api/**").hasRole("USER")
 //                    .antMatchers("/error").permitAll()
                     .anyRequest().authenticated())
-                .addFilterAt(restAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults())
                 ;
-    }
-
-    private RestAuthenticationFilter restAuthenticationFilter() throws Exception {
-        RestAuthenticationFilter filter = new RestAuthenticationFilter(objectMapper);
-        filter.setAuthenticationSuccessHandler(jsonLoginSuccessHandler());
-        filter.setAuthenticationFailureHandler(jsonLoginFailureHandler());
-        filter.setAuthenticationManager(authenticationManager());
-        filter.setFilterProcessesUrl("/authorize/login");
-        return filter;
-    }
-
-    private static LogoutSuccessHandler jsonLogoutSuccessHandler() {
-        return (request, response, authentication) -> {
-            if (authentication != null && authentication.getDetails() != null) {
-                request.getSession().invalidate();
-            }
-            response.setStatus(HttpStatus.OK.value());
-            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-            response.setCharacterEncoding("UTF-8");
-            response.getWriter().write(new ObjectMapper().writeValueAsString(Map.of("msg", "Logout success!")));
-            log.debug("Logout success!");
-        };
-    }
-
-    private AuthenticationFailureHandler jsonLoginFailureHandler() {
-        return (request, response, e) -> {
-            response.setStatus(HttpStatus.UNAUTHORIZED.value());
-            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-            response.setCharacterEncoding("UTF-8");
-            Map<String, String> errData = Map.of("title", "auth failed", "details", e.getMessage());
-            ObjectMapper objectMapper = new ObjectMapper();
-            response.getWriter().println(objectMapper.writeValueAsString(errData));
-        };
-    }
-
-    private AuthenticationSuccessHandler jsonLoginSuccessHandler() {
-        return (request, response, authentication) -> {
-            ObjectMapper objectMapper = new ObjectMapper();
-            response.setStatus(HttpStatus.OK.value());
-            response.getWriter().println(objectMapper.writeValueAsString(authentication));
-            log.debug("Auth successful");
-        };
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .userDetailsService(userDetailsService)
-                .passwordEncoder(passwordEncoder());
+                .passwordEncoder(passwordEncoder())
+                .userDetailsPasswordManager(userDetailsPasswordService);
     }
 
     @Bean
