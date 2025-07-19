a89980f9c75ce6e9f21b258bfa168ddaa629b20f
kennyyiu <kenny.kwyiu@gmail.com>
2025-07-20 00:03:16 +0800

    JwtUtil - createJWT

---
+++ src/main/java/com/kenny/uaa/util/JwtUtil.java
@@ -0,0 +1,31 @@
+package com.kenny.uaa.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Component
+public class JwtUtil {
+    public static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+    public String createJWT(UserDetails userDetails) {
+        long now = System.currentTimeMillis();
+        return Jwts.builder()
+                .setId("kenny")
+                .claim("authorities", userDetails.getAuthorities()
+                        .stream()
+                        .map(GrantedAuthority::getAuthority
+                        ).collect(Collectors.toList()))
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(now))
+                .setExpiration(new Date(now + 60_000))
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+    }
+}
