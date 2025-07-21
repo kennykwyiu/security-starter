0ff05dc881dc9f4d3f42d8e64ae07ab90a3ccffd
kennyyiu <kenny.kwyiu@gmail.com>
2025-07-21 20:53:33 +0800

    add createRefreshToken and createJwtToken

--- src/main/java/com/kenny/uaa/util/JwtUtil.java
+++ src/main/java/com/kenny/uaa/util/JwtUtil.java
@@ -1,8 +1,10 @@
 package com.kenny.uaa.util;
 
+import com.kenny.uaa.config.AppProperties;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
@@ -11,20 +13,27 @@ import java.security.Key;
 import java.util.Date;
 import java.util.stream.Collectors;
 
+@RequiredArgsConstructor
 @Component
 public class JwtUtil {
     public static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
     public static final Key refreshKey = Keys.secretKeyFor(SignatureAlgorithm.HS512);
 
+    private final AppProperties appProperties;
+
     public String createAccessToken(UserDetails userDetails) {
-        return createJwtToken(userDetails, key);
+        return createJwtToken(userDetails,
+                appProperties.getJwt().getAccessTokenExpireTime(),
+                key);
     }
 
     public String createRefreshToken(UserDetails userDetails) {
-        return createJwtToken(userDetails, refreshKey);
+        return createJwtToken(userDetails,
+                appProperties.getJwt().getRefreshTokenExpireTime(),
+                refreshKey);
     }
 
-    public String createJwtToken(UserDetails userDetails, Key key) {
+    public String createJwtToken(UserDetails userDetails, long timeToExpire, Key key) {
         long now = System.currentTimeMillis();
         return Jwts.builder()
                 .setId("kenny")
@@ -34,7 +43,7 @@ public class JwtUtil {
                         ).collect(Collectors.toList()))
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(now))
-                .setExpiration(new Date(now + 60_000))
+                .setExpiration(new Date(now + timeToExpire))
                 .signWith(key, SignatureAlgorithm.HS512)
                 .compact();
     }
