1e25ec0c945f10b9a38af1a108632ce3db54c7ea
kennyyiu <kenny.kwyiu@gmail.com>
2025-07-24 21:48:02 +0800

    PATTERN_MOBILE

--- src/main/java/com/kenny/uaa/config/Constants.java
+++ src/main/java/com/kenny/uaa/config/Constants.java
@@ -2,4 +2,20 @@ package com.kenny.uaa.config;
 
 public class Constants {
     public static final String PROBLEM_BASE_URI = "http://localhost:8080";
+    /*
+     * - Need to update PATTERN_MOBILE to match Hong Kong mobile numbers:
+
+        - Starts with 5, 6, or 9
+        - 8 digits total
+        - Optional +852 country code
+
+        - New regex should validate:
+
+        - Numbers like 51234567, 61234567, 91234567
+        - Or +85251234567
+
+        - Should maintain the existing PROBLEM_BASE_URI constant
+
+     */
+    public static final String PATTERN_MOBILE = "^(\\+852)?[569]\\d{7}$";
 }
