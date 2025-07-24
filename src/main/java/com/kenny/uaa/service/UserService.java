5586e661a68ad202eb1a019a9650ea9e682991f1
kennyyiu <kenny.kwyiu@gmail.com>
2025-07-24 22:07:48 +0800

    register

--- src/main/java/com/kenny/uaa/service/UserService.java
+++ src/main/java/com/kenny/uaa/service/UserService.java
@@ -1,6 +1,9 @@
 package com.kenny.uaa.service;
 
+import com.kenny.uaa.config.Constants;
 import com.kenny.uaa.domain.Auth;
+import com.kenny.uaa.domain.User;
+import com.kenny.uaa.repository.RoleRepo;
 import com.kenny.uaa.repository.UserRepo;
 import com.kenny.uaa.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
@@ -8,10 +11,14 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
 @RequiredArgsConstructor
 @Service
 public class UserService {
     private final UserRepo userRepo;
+    private final RoleRepo roleRepo;
     private final PasswordEncoder passwordEncoder;
     private final JwtUtil jwtUtil;
 
@@ -36,5 +43,15 @@ public class UserService {
 
     public boolean isMobileExist(String mobile) {
         return userRepo.countByMobile(mobile) > 0;
+    }
+
+    public User register(User user) {
+        return roleRepo.findOptionalByAuthority(Constants.ROLE_USER)
+                .map(role -> {
+                    User userToSave = user.withAuthorities(Set.of(role))
+                            .withPassword(passwordEncoder.encode(user.getPassword()));
+                    return userToSave;
+                })
+                .orElseThrow();
     }
 }
