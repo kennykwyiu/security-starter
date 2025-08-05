b73e3409a6a90125e94440c3cdf5be43ffe3cf96
kennyyiu <kenny.kwyiu@gmail.com>
2025-08-05 14:46:00 +0800

    UserCacheService - cacheUser

---
+++ src/main/java/com/kenny/uaa/service/UserCacheService.java
@@ -0,0 +1,33 @@
+package com.kenny.uaa.service;
+
+import com.kenny.uaa.config.Constants;
+import com.kenny.uaa.domain.User;
+import com.kenny.uaa.util.CryptoUtil;
+import com.kenny.uaa.util.TotpUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RMapCache;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@RequiredArgsConstructor
+@Slf4j
+@Service
+public class UserCacheService {
+    private final RedissonClient redisson;
+    private final TotpUtil totpUtil;
+    private final CryptoUtil cryptoUtil;
+
+    public String cacheUser(User user) {
+        String mfaId = cryptoUtil.randomAlphanumeric(12);
+        RMapCache<String, User> cache = redisson.getMapCache(Constants.CACHE_MFA);
+        if (!cache.containsKey(mfaId)) {
+            cache.put(mfaId, user, totpUtil.getTimeStepInLong(), TimeUnit.SECONDS);
+        }
+        return mfaId;
+    }
+
+
+}
